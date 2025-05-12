f035ebdee06ed81b990b0c93359d12fed86cadc3
egoska123 <igorpocht1@gmail.com>
2025-05-12 17:27:38 +0300

    feature/task-265-new-component-community-card

--- src/constants/Fonts.tsx
+++ src/constants/Fonts.tsx
@@ -1,4 +1,27 @@
-export const FONTS = {
-    VK_SANS_REGULAR: 'VK Sans Display Expanded-Regular',
-  };
-  
+import VKSansExpanded from "../assets/fonts/VK Sans Display Expanded Regular.ttf"
+import VKSansDisplay from "../assets/fonts/VKSansDisplay-Regular.ttf"
+
+export const Fonts = () => (
+  <style>
+    {`
+      @font-face {
+        font-family: 'VK Sans Display Expanded';
+        src: url(${VKSansExpanded}) format('truetype');
+        font-weight: 400;
+        font-style: normal;
+      }
+
+      @font-face {
+        font-family: 'VK Sans Display';
+        src: url(${VKSansDisplay}) format('truetype');
+        font-weight: 700;
+        font-style: normal;
+      }
+
+      body {
+        font-family: 'VK Sans Display Expanded';
+        font-family: 'VK Sans Display';
+      }
+    `}
+  </style>
+);
